9cc8eeeadb54c10a28b8d69df369b7ebc924c661
orca8 <orca8@users.noreply.github.com>
2014-04-20 20:31:17 +0900

    Add test environment for database
    test environment database using inmemory SQLite.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,7 +1,34 @@
 <?php
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \Illuminate\Foundation\Testing\TestCase::setUp()
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->prepareForTests();
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see PHPUnit_Framework_TestCase::tearDown()
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+        Artisan::call('migrate:reset');
+        DB::disconnect();
+    }
 
     /**
      * Creates the application.
@@ -15,5 +42,14 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
         $testEnvironment = 'testing';
 
         return require __DIR__ . '/../../bootstrap/start.php';
+    }
+
+    public function prepareForTests()
+    {
+        Artisan::call('migrate');
+        $this->seed();
+        Mail::pretend(true);
+        // テスト実行時にルートフィルターを有効にする
+        Route::enableFilters();
     }
 }
