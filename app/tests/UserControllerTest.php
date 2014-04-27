07b57fc34dc0610f4c3cc90354c2ba6af964a9eb
orca8 <orca8@users.noreply.github.com>
2014-04-27 13:07:01 +0900

    close #4 Add require-dev package way/laravel-test-helpers
    Laravelでのテストをサポートするパッケージ。
    `composer require`でパッケージを追加インストール。
    `php composer.phar require --dev way/laravel-test-helpers:dev-master`

--- app/tests/UserControllerTest.php
+++ app/tests/UserControllerTest.php
@@ -2,6 +2,7 @@
 
 class UserControllerTest extends TestCase
 {
+    use Way\Tests\ControllerHelpers;
 
     public function setUp()
     {
@@ -18,5 +19,16 @@ class UserControllerTest extends TestCase
 
         // 検証
         $this->assertResponseOk();
+        $this->see('foo');
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function showNoProfile()
+    {
+        // 実行
+        $this->call('GET', 'user/');
     }
 }
